85b03666c168b9e9edfa6f5577131afa5a51cb3a
amir <amir@gmail.com>
2025-11-20 18:35:15 -0800

    Built the starter page of the virtual meeting room

---
+++ virtualmeeting/src/components/startButton/startButton.jsx
@@ -0,0 +1,10 @@
+import './startButton.css'
+
+function StartButton(){
+    return(
+    <div className='startBox'>
+        <h2>Ready to start the interview?</h2>
+    </div>);
+}
+
+export default StartButton
