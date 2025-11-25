6a531f3481943bdd3597f6da6d41eecd35535300
amir <amir@gmail.com>
2025-11-25 15:23:52 -0800

    Added the logos and added the main logo to virtual space

--- virtualmeeting/src/App.js
+++ virtualmeeting/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect } from 'react';
 import MockInterview from './components/mockInterview/mockInterview';
+import logo from "./main_logo-removebg.png";
 
 function App() {
   useEffect(() => {
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <div className='header'>
-
+          <img alt='logo' src={logo} className='mainLogo' />
       </div>
       <div className='virtualSpace'>
         <MockInterview/>
