ef43e4550c07663f57d3db9205f161dbaf0a9999
amir <amir@gmail.com>
2025-12-12 17:36:18 -0800

    Changed the react app favicon to simpupro logo

--- virtualmeeting/src/index.js
+++ virtualmeeting/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import icon from "./icon_logo.png";
+
+const setFavicon = (faviconUrl) => {
+  const link =
+    document.querySelector("link[rel~='icon']") || document.createElement('link');
+  link.rel = 'icon';
+  link.href = faviconUrl;
+  document.getElementsByTagName('head')[0].appendChild(link);
+};
+
+setFavicon(icon);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
